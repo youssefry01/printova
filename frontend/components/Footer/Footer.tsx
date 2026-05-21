a556cde754ee209bcb20b2bf31dd7335bc46f63e
youssefry01 <youssefry01@gmail.com>
2026-05-21 14:43:12 +0300

    Fix Footer Image

--- frontend/components/Footer/Footer.tsx
+++ frontend/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: FC = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="flex flex-col items-center justify-center w-full py-2 lg:py-10 bg-linear-to-b from-[#5524B7] to-[#380B60] text-white/70">
-      <Image className="size-24 lg:size-32" src="/logo.png" width={96} height={96} alt='logo' />
+      <Image className="size-24 lg:size-32" src="/logo.png" width={128} height={128} alt='logo' />
         <p className="text-sm lg:text-base my-2 text-center">Copyright © {currentYear} Printova. All rights reservered.</p>
     </footer>
   )
